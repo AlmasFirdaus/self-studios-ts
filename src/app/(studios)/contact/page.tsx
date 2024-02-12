35a078a08211183a17835fbb26002a67b92bd6c9
AlmasFirdaus <firdausalmas22@gmail.com>
2024-02-12 12:40:39 +0700

    finishing contact page

--- src/app/(studios)/contact/page.tsx
+++ src/app/(studios)/contact/page.tsx
@@ -10,6 +10,7 @@ import { z } from 'zod';
 import { Button } from '@/components/ui/button';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { AspectRatio } from '@/components/ui/aspect-ratio';
 
 type messageFormType = z.infer<typeof messageFormSchema>;
 
@@ -32,7 +33,7 @@ const Page = () => {
   };
 
   return (
-    <section className="mt-14">
+    <section className="mt-14 flex flex-col gap-10 lg:gap-16">
       <div className="flex flex-col justify-center items-center">
         <h1 className="font-semibold lg:font-bold text-3xl lg:text-5xl/relaxed my-2 ">Contact Us</h1>
         <div className="mx-8 lg:mx-0 lg:w-5/12 font-sans text-sm lg:text-lg/relaxed text-center mt-6 space-y-4">
@@ -40,7 +41,7 @@ const Page = () => {
         </div>
       </div>
 
-      <div className="w-full flex flex-col lg:flex-row my-14">
+      <div className="w-full flex flex-col lg:flex-row">
         <div className="w-full lg:w-1/2 aspect-video lg:aspect-square relative">
           <Image src={'/images/girls.jpg'} alt={'girls'} sizes="100%,100%" className="aspect-video lg:aspect-square object-cover grayscale" fill priority />
         </div>
@@ -125,6 +126,31 @@ const Page = () => {
             </div>
           </div>
         </div>
+      </div>
+      <div className="w-full min-h-56 px-10 bg-gray-100 flex flex-col lg:flex-row justify-evenly items-start lg:items-center">
+        <h3 className="font-semibold mt-14 lg:mt-0 text-2xl/relaxed lg:text-4xl/relaxed">Opening Hours</h3>
+        <div className="grid grid-cols-1 gap-y-8 gap-x-4 my-10 lg:my-0 leading-relaxed">
+          <div className="w-full flex flex-col lg:flex-row lg:justify-between lg:items-center lg:gap-x-12">
+            <p>Mon - Fri</p>
+            <p>9.00 am - 8.00 pm</p>
+          </div>
+          <div className="w-full flex flex-col lg:flex-row lg:justify-between lg:items-center lg:gap-x-12">
+            <p>Saturday & Sunday</p>
+            <p>9.00 am - 9.00 pm</p>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d246.96791950473968!2d112.6597699568123!3d-7.9485499295027955!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd6296dd2e398f5%3A0xaa3fdcb12280190c!2sPondok%20Indah%20Estate!5e0!3m2!1sid!2sid!4v1707715986389!5m2!1sid!2sid"
+          width="600"
+          height="450"
+          className="w-full"
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
       </div>
     </section>
   );
