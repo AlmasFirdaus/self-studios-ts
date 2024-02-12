07897512fc6c7854cb487c776fc940f1a1c04739
AlmasFirdaus <firdausalmas22@gmail.com>
2024-02-12 16:22:34 +0700

    fixing pricelist

--- src/components/pricelist/pricelist-option.tsx
+++ src/components/pricelist/pricelist-option.tsx
@@ -13,8 +13,8 @@ const PricelistOption = ({ options }: Props) => {
           <h4 className="inline-flex flex-1 font-semibold">{option.name}</h4>
           <div className="flex flex-col justify-between leading-relaxed gap-2">
             {option.selected?.map((select, index) => (
-              <div key={index} className="flex items-center gap-x-10">
-                <p className="flex flex-1">- {select.title}</p>
+              <div key={index} className="flex items-center lg:gap-x-10 gap-2">
+                <p className="w-full flex lg:flex-1">- {select.title}</p>
                 <div className="w-32 h-[1px] bg-black" />
                 <p className="flex w-8 justify-end items-end">{select.price_selected}</p>
               </div>
--- src/components/pricelist/pricelist-rules.tsx
+++ src/components/pricelist/pricelist-rules.tsx
@@ -7,12 +7,12 @@ type Props = {
 
 const PricelistRules = ({ rules, price }: Props) => {
   return (
-    <div className="w-full my-4 flex justify-end gap-10">
-      <div className="w-full flex justify-between items-center leading-relaxed gap-10">
-        <div className="inline-flex flex-1" />
+    <div className="w-full my-4 flex lg:justify-end lg:gap-10">
+      <div className="w-full flex justify-between items-center leading-relaxed gap-2 lg:gap-10">
+        <div className="hidden lg:inline-flex lg:flex-1" />
         <div className="flex flex-col gap-x-10 gap-2">
           {rules?.map((rule, index) => (
-            <div key={index} className="flex flex-col flex-1">
+            <div key={index} className="w-full flex flex-col lg:flex-1">
               <p>- {rule}</p>
             </div>
           ))}
