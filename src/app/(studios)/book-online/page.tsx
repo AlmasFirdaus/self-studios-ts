e43b3df243f907ffdd1e2355f657e2b5036e95fb
AlmasFirdaus <firdausalmas22@gmail.com>
2024-03-25 11:48:29 +0700

    adjusting responsive design

--- src/app/(studios)/book-online/page.tsx
+++ src/app/(studios)/book-online/page.tsx
@@ -7,7 +7,7 @@ const page = () => {
     <section className='min-h-screen'>
       <h1 className='font-bold text-center text-3xl md:text-5xl/relaxed mb-16'>Our Studios Location</h1>
       <div className='flex'>
-        <LocationShowcase studios={studios} gridConfig='grid-cols-1 md:grid-cols-3' />
+        <LocationShowcase studios={studios} gridConfig='grid-cols-1 md:grid-cols-2 lg:grid-cols-3' />
       </div>
     </section>
   );
